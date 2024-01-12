feeda076f461e9836f3346e62057e12e1c001001
JasonSt12 <103218916+JasonSt12@users.noreply.github.com>
2024-01-12 16:10:13 -0500

    Finished section 2

---
+++ frontend/src/products.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description: `Bluestooth technology lets you connect it with compatible devies
+    wirelessly High-quality AAC audio offers immersive listening
+    experience Built-in microphone allows you to take calls while
+    working`,
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+
+  {
+    _id: "6",
+    name: "Amazon Echo Dot 3rd Generation",
+    image: "/images/alexa.jpg",
+    description: `Meet the echo dot - Our most popular smart speaker with a fabric
+    design. It is our most compact smart speaker that fits perfectly
+    into small spaces`,
+    brand: "Amazon",
+    category: "Electronics",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
